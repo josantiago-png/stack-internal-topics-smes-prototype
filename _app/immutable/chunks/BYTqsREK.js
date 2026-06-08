260594a2c3fa29601d06565f4d74a025fe539f5a
josantiago-png <josantiago@stackoverflow.com>
2026-06-08 13:28:43 -0400

    Update _app/immutable/chunks/BYTqsREK.js

---
+++ _app/immutable/chunks/BYTqsREK.js
@@ -0,0 +1,1 @@
+import{p as r}from"./CdV_gCY_.js";import{s as t}from"./BX5bsXBY.js";const e={get error(){return r.error},get params(){return r.params},get route(){return r.route},get status(){return r.status},get url(){return r.url}};t.updated.check;const o=e;export{o as p};
